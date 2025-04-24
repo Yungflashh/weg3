55bfe7791ebc958fd476724d79ac2e735eab4e3b
Glitch4real <144991787+Glitch4real@users.noreply.github.com>
2025-04-24 22:51:57 +0100

    link nav button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({icon_image,text,btn_icon,btn_text}) => {
         <img className=' w-[90px] h-[90px]' src={icon_image} alt="" />
         <div className='grid gap-4'>
             <p className=' text-[18px] font-[700]'>{text}</p>
-            <button className=' w-[max-content] flex items-center justify-center gap-2 bg-blue-600 py-2 px-5 rounded-3xl text-white font-medium'>
+            <button className=' w-[max-content] flex items-center justify-center gap-2 bg-gradient-to-r from-[#85C0FF] to-[#034afd] py-2 px-5 rounded-3xl text-white font-medium hover:'>
               {btn_icon} {btn_text}
               </button>
         </div>
--- src/components/static/Header/Header.jsx
+++ src/components/static/Header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { PiWalletFill } from "react-icons/pi";
 import wallet_logo from "../../../assets/wallet_logo.jpg"
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header-container flex justify-between p-5 '>
+    <div className='header-container flex justify-between p-5 bg-white'>
         <div className="logo-div">
-
+        <Link to='manual'>
         <button className='flex justify-center items-center border-2 border-gray-200 px-4 py-2 rounded-3xl gap-1 urbanist text-[14px]'> <PiWalletFill color='#45E8FF'/> Connect Wallet</button> 
-
+        </Link>
 
         </div>
 
