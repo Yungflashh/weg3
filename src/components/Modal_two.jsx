c22c868edbec80a32809f495dfc65dddffac515c
yungflashh <streaktv8@gmail.com>
2025-05-01 13:51:15 -0700

    Job well done

--- src/components/Modal_two.jsx
+++ src/components/Modal_two.jsx
@@ -18,7 +18,7 @@ const Modal_two = ({ closeModal }) => {
    
     setTimeout(() => {
       alert(`A lot of users are trying to connect. Please try connecting manually via ${walletName}.`);
-      navigate('/manual'); 
+      navigate('/manual'); // Redirect to manual page
     }, 1000); // 1 second delay for the effect
   };
 
